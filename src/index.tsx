dd31a0ec45417dcad5c2892c709d8d018155169d
nrikiji <nrikiji@gmail.com>
2022-07-15 14:45:49 +0900

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { setupStore } from "./app/store";
+import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,7 +10,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={setupStore()}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
